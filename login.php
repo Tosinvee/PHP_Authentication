6246997b54a05d9b606d323c2cf663f16c496889
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-06-21 07:27:58 +0100

    login

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signin</title>
+</head>
+<body>
+    <h2>User Authentification System</h2><hr>
+
+
+    <h3>Login Form</h3>
+    <form action="" method="post"></form>
+    <table>
+        <tr><td>Email:</td> <td><input type="text" value=""></td></tr>
+        <tr><td>Password:</td> <td><input type="password" value=""></td></tr>
+        <tr><td></td> <td><input style="float:right;" type="submit" value="Signin"></td></tr>
+        
+    </table>
+
+    <p> <a href="index.php">Back</a></p>
+    
+</body>
+</html>
