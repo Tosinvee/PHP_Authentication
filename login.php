70e8e5f90e9e121a9d3ea0f47a1b6710e2648a75
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-06-24 07:19:09 +0100

    login/session

--- login.php
+++ login.php
@@ -1,3 +1,58 @@
+<?php 
+include_once 'resources/session.php';
+include_once 'resources/database.php';
+include_once 'resources/validate.php';
+
+//if the loginbtn is clicked process the form
+if(isset($_POST['loginBtn'])){
+    //arrays to hold errors
+    $form_errors =array();
+
+    //validate form 
+$required_field = array('email', 'password');
+
+$form_errors = array_merge($form_errors, check_empty_fields($required_field));
+
+if(empty($form_errors)){
+    //collect form from data
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    //check if user exist in the database
+    $sqlQuery = "SELECT * FROM users WHERE email = :email";
+    $statement =$db->prepare($sqlQuery);
+    $statement->execute(array(':email' => $email));
+
+    while($row = $statement->fetch()){
+        $id = $row['id'];
+        $hashed_password = $row ['password'];
+        $email = $row['username'];
+
+        if(password_verify($password, $hashed_password)){
+            $_SESSION['id'] = $id;
+            $_SESSION['email'] =$email;
+            header("location: index.php");
+            exit();
+        }else{
+            $result ="<p style='padding: 20px; color: red; border: 1px solid gray;'> Invalid usernme or password</p>";
+        }
+        
+    }
+}else{
+    if(count($form_errors) == 1){
+        $result = "<p style='color:red;'> There was an error in the form <br>";
+
+   }else{
+    $result = "<p style='color:red;'>There were" .count($form_errors). "errors in the form <br>";
+
+   }
+    }
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,11 +65,15 @@
 
 
     <h3>Login Form</h3>
+
+    <?php if(isset($result)) echo $result; ?>
+    <?php if(!empty($form_errors)) echo show_errors($form_errors) ?>
+
     <form action="" method="post"></form>
     <table>
-        <tr><td>Email:</td> <td><input type="text" value=""></td></tr>
-        <tr><td>Password:</td> <td><input type="password" value=""></td></tr>
-        <tr><td></td> <td><input style="float:right;" type="submit" value="Signin"></td></tr>
+        <tr><td>Email:</td> <td><input type="text" name "email" value=""></td></tr>
+        <tr><td>Password:</td> <td><input type="password" name="password" value=""></td></tr>
+        <tr><td></td> <td><input style="float:right;" type="submit" name="loginBtn" value="Signin"></td></tr>
         
     </table>
 
